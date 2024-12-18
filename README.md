0285f9f18f9d23018ebf6be52b5290a138b32c0e
Rohan KumarPC <rkumar51@horizon.csueastbay.edu>
2024-12-17 16:19:21 -0800

    [FIX] fixed scaling bug. [DOCS] modified README.md

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 
 
 >to run:<br><br>
-javac .\com\example\ticTacToeApp\*.java // compile all java files<br>
+javac .\com\example\ticTacToeApp\\*.java // compile all java files<br>
 java com.example.ticTacToeApp.TicTacToeDriver
 
 >To make .jar file go to project directory<br>
 run:<br><br>
-jar cvfm TicTacToe.jar .\META-INF\MANIFEST.MF .\com\example\ticTacToeApp\*.class
+jar cvfm TicTacToe.jar .\META-INF\MANIFEST.MF .\com\example\ticTacToeApp\\*.class
 
 >manifest.mf contents:<br>
 Manifest-Version: 1.0 //version number <br>
--- com/example/ticTacToeApp/TicTacToeGame.java
+++ com/example/ticTacToeApp/TicTacToeGame.java
@@ -13,16 +13,17 @@ import javax.swing.JOptionPane;
 
 public class TicTacToeGame {
     private final int NUMBER_OF_BUTTONS = 9;
-    private final int FRAME_SIZE = 900;
-    private final int BUTTON_SIZE = 300;
+    private final int FRAME_WIDTH = 900;
+    private final int FRAME_HEIGHT = 750;
+    private int BUTTON_SIZE = 300;
     private static char turn = 'X';
+    JFrame frame = new JFrame();
+    JButton[] buttons = new JButton[NUMBER_OF_BUTTONS];
 
     public void play(){
-        JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(FRAME_SIZE, FRAME_SIZE);
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setLayout(new java.awt.GridLayout(3, 3, 0, 0));
-        JButton[] buttons = new JButton[NUMBER_OF_BUTTONS];
 
         for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
             buttons[i] = new JButton();
@@ -36,6 +37,7 @@ public class TicTacToeGame {
                 public void actionPerformed(java.awt.event.ActionEvent e) {
                     JButton button = (JButton) e.getSource();
                     button.setFont(new Font("Arial", Font.BOLD, BUTTON_SIZE));
+                    button.setForeground(Color.BLACK);
                     if (turn == 'X' && button.getText().equals("")){
                         button.setText("X");
                         turn = 'O';
@@ -54,7 +56,8 @@ public class TicTacToeGame {
             @Override
             public void componentResized(ComponentEvent e) {
                 int newFontSize = Math.min(frame.getWidth(), frame.getHeight()) / 3; // Adjust this factor as needed
-                Font newFont = new Font("Arial", Font.BOLD, newFontSize);
+                BUTTON_SIZE = newFontSize;
+                Font newFont = new Font("Arial", Font.BOLD, BUTTON_SIZE);
                 for (JButton button : buttons) {
                     button.setFont(newFont);
                 }
@@ -62,6 +65,7 @@ public class TicTacToeGame {
         });
 
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
 
 
@@ -69,31 +73,31 @@ public class TicTacToeGame {
         int i = 0, j = 1, k = 2;
         for (; i < NUMBER_OF_BUTTONS; i+=3, j+=3, k+=3){ // check rows
             if (buttons[i].getText() == "X" && buttons[j].getText() == "X" && buttons[k].getText() == "X"){
-                showVictoryMessage(frame, true, false, i, j, k, buttons);
+                showVictoryMessage(true, false, i, j, k);
             }
             else if (buttons[i].getText() == "O" && buttons[j].getText() == "O" && buttons[k].getText() == "O"){
-                showVictoryMessage(frame, false, true, i, j, k, buttons);
+                showVictoryMessage(false, true, i, j, k);
             }
         }
         for (i = 0, j = 3, k = 6; i < 3; i++, j++, k++){ // check columns
             if (buttons[i].getText() == "X" && buttons[j].getText() == "X" && buttons[k].getText() == "X"){
-                showVictoryMessage(frame, true, false, i, j, k, buttons);
+                showVictoryMessage(true, false, i, j, k);
             }
             else if (buttons[i].getText() == "O" && buttons[j].getText() == "O" && buttons[k].getText() == "O"){
-                showVictoryMessage(frame, false, true, i, j, k, buttons);
+                showVictoryMessage(false, true, i, j, k);
             }
         }
         if (buttons[0].getText() == "X" && buttons[4].getText() == "X" && buttons[8].getText() == "X") { // first diagonal
-            showVictoryMessage(frame, true, false, 0, 4, 8, buttons);
+            showVictoryMessage(true, false, 0, 4, 8);
         }
         else if (buttons[0].getText() == "O" && buttons[4].getText() == "O" && buttons[8].getText() == "O"){
-            showVictoryMessage(frame, false, true, 0, 4, 8, buttons);
+            showVictoryMessage(false, true, 0, 4, 8);
         }
         if (buttons[2].getText() == "X" && buttons[4].getText() == "X" && buttons[6].getText() == "X"){ // second diagonal
-            showVictoryMessage(frame, true, false, 2, 4, 6, buttons);
+            showVictoryMessage(true, false, 2, 4, 6);
         }
         else if (buttons[2].getText() == "O" && buttons[4].getText() == "O" && buttons[6].getText() == "O"){
-            showVictoryMessage(frame, false, true, 2, 4, 6, buttons);
+            showVictoryMessage(false, true, 2, 4, 6);
         }
 
         for (i = 0; i < NUMBER_OF_BUTTONS; i++){ // check for draw
@@ -101,47 +105,47 @@ public class TicTacToeGame {
                 break;
             }
             else if (i == NUMBER_OF_BUTTONS - 1 && (buttons[i].getText() == "X" || buttons[i].getText() == "O")){
-                showVictoryMessage(frame, false, false, i, j, k, buttons);
+                showVictoryMessage(false, false, i, j, k);
             }
         }
     }
 
 
-    private void showVictoryMessage(JFrame frame, Boolean playerXwon, Boolean playerOwon, int i, int j, int k, JButton[] buttons){
+    private void showVictoryMessage(Boolean playerXwon, Boolean playerOwon, int i, int j, int k){
         if (playerXwon && !playerOwon){
-            color(buttons, i, j, k);
-            JOptionPane.showMessageDialog(frame, "Player X wins", "Winner", JOptionPane.INFORMATION_MESSAGE);
-            reset(buttons);
+            color(i, j, k);
+            JOptionPane.showMessageDialog(frame, "Player X won", "Winner", JOptionPane.INFORMATION_MESSAGE);
+            reset();
         } else if (playerOwon && !playerXwon){
-            color(buttons, i, j, k);
-            JOptionPane.showMessageDialog(frame, "Player O wins", "Winner", JOptionPane.INFORMATION_MESSAGE);
-            reset(buttons);
+            color(i, j, k);
+            JOptionPane.showMessageDialog(frame, "Player O won", "Winner", JOptionPane.INFORMATION_MESSAGE);
+            reset();
         } else if (!playerXwon && !playerOwon){
             JOptionPane.showMessageDialog(frame, "It's a draw", "Draw", JOptionPane.INFORMATION_MESSAGE);
-            reset(buttons);
+            reset();
         }
     }
 
 
-    private void unColor(JButton[] buttons){
+    private void unColor(){
         for (int i = 0; i < buttons.length; i++){
             buttons[i].setForeground(Color.BLACK);
         }
     }
 
 
-    private void color(JButton[] buttons, int i, int j, int k){
+    private void color(int i, int j, int k){
         buttons[i].setForeground(Color.GREEN);
         buttons[j].setForeground(Color.GREEN);
         buttons[k].setForeground(Color.GREEN);
     }
 
     
-    private void reset(JButton[] buttons){
+    private void reset(){
         for (int i = 0; i < buttons.length; i++){
             buttons[i].setText("");
         }
         turn = 'X';
-        unColor(buttons);
+        unColor();
     }
 }
